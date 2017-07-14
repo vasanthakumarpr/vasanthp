3cf84c63930ae4e99064d8790cf77ccd375c198e
vasanthakumarpr <vasanthp97@gmail.com>
2017-07-14 13:49:54 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class reverse 
+{
+
+	 	public static void main(String[] args)
+	 	{
+	 		Scanner in=new Scanner(System.in);
+	 		System.out.println("Enter the String");
+	 		String s=in.next();
+	 	    StringBuffer s1=new StringBuffer(s);
+		    StringBuffer s2=s1.reverse();
+	 		System.out.println(s2);
+
+}
+}
